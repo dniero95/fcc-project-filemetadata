9e6844c2b5442c6dbfca4e65183448e408a5514b
dniero95 <darioniero@icloud.com>
2023-01-26 02:20:19 +0100

    add post /api/fileanalyse endpoint

--- index.js
+++ index.js
@@ -5,8 +5,15 @@ require('dotenv').config()
 // add body parser
 const bodyParser = require('body-parser');
 
+// to handle the file upload I use the code in
+// https://pqina.nl/blog/upload-image-with-nodejs/
+// handle file upload
+const fileUpload = require('express-fileupload');
+
 var app = express();
 
+// Use the express-fileupload middleware
+app.use(fileUpload());
 // bodyParser 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,7 +26,15 @@ app.get('/', function (req, res) {
 });
 
 
-
+app.post('/api/fileanalyse', (req, res) => {
+  res.json(
+    {
+      name: req.files.upfile.name,
+      type: req.files.upfile.mimetype,
+      size: req.files.upfile.size
+    }
+  );
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
